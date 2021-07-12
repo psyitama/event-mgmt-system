80204f7d88be7525691999c3f694f10696ba2b07
psyitama <psyrone.agpalo@yahoo.com>
2021-07-12 16:12:06 +0800

    Add validation: Don't allow date in the past (add event feature)

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,5 +7,12 @@ class Event < ApplicationRecord
   # Validations
   validates :name, :date, :location, :state_id, presence: true
   validates :name, length: {minimum: 2}
-  
+  validate :expiration_date_cannot_be_in_the_past
+
+  private
+  def expiration_date_cannot_be_in_the_past
+    if date.present? && date < Date.today
+      errors.add(:date, "can't be in the past")
+    end
+  end
 end
